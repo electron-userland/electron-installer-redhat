060071451a5859a01eaf0724a685aed21bf0762f
Felipe Castillo <1280530+fcastilloec@users.noreply.github.com>
2020-06-28 13:35:56 -0700

    feat: add dependencies for Electron >= 9 (#156)
    Co-authored-by: Mark Lee <electronjs@lazymalevolence.com>

--- src/dependencies.js
+++ src/dependencies.js
@@ -6,6 +6,8 @@ const spawn = require('./spawn')
 
 const dependencyMap = {
   atspi: 'at-spi2-core',
+  drm: 'libdrm',
+  gbm: 'mesa-libgbm',
   gconf: 'GConf2',
   glib2: 'glib2',
   gtk2: 'gtk2',
@@ -17,6 +19,7 @@ const dependencyMap = {
   nss: '(nss or mozilla-nss)',
   trashCli: 'trash-cli',
   uuid: '(libuuid or libuuid1)',
+  xcbDri3: 'libxcb',
   xdgUtils: 'xdg-utils',
   xss: 'libXScrnSaver',
   xtst: '(libXtst or libXtst6)'
